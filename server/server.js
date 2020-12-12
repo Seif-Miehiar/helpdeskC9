c30953753a5b39f3760a8d93913648ad6888543d
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-12-12 21:09:13 +0200

    init

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3500;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+// parse requests of content-type: application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+	res.json({ message: "Welcome to Help Desk application." });
 	// res.send("hello World Express!");
 });
 
